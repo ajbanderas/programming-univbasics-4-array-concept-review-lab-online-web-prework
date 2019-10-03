6f28701e7e21b96a0cad8bdf88f8a2e5d617e458
Kevin Downs <kevin.downs04@gmail.com>
2019-10-03 00:12:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,7 +19,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  value = 0
+  value = array[0]
     array.length.times do |index|
       if value > array[index]
         value = array[index]
